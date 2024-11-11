81f690463936dc23d4841d46212858d5c54cd9fe
ZalokNessi <0022410027@alumnos.univa.mx>
2024-11-11 13:26:16 -0600

    Add files via upload

---
+++ Pasoapasoo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pasoapasoo;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Pasoapasoo extends JFrame{
+    
+    public Pasoapasoo(){
+        setTitle("Copiar Pegar");
+        setSize(350,300);
+        setLocationRelativeTo(null);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Pasoapaso ventana = new Pasoapaso(); 
+        ventana.setVisible(true);
+        
+        
+    }
+    
+}
